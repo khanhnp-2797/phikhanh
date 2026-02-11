f35b29013bdb5173d48558d2be19ebbe44a0e9d8
nguyen.phi.khanh <nguyen.phi.khanh@sun-asterisk.com>
2026-02-11 08:35:23 +0700

    chore: remove redundant

--- repositories/user/user_repository.go
+++ repositories/user/user_repository.go
@@ -1,8 +1,6 @@
 package user
 
 import (
-	"phikhanh/models"
-
 	"gorm.io/gorm"
 )
 
@@ -12,16 +10,4 @@ type UserRepository struct {
 
 func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
-}
-
-func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
-	var user models.User
-	err := r.db.Where("email = ?", email).First(&user).Error
-	return &user, err
-}
-
-func (r *UserRepository) CountActive() (int64, error) {
-	var count int64
-	err := r.db.Model(&models.User{}).Where("is_active = ?", true).Count(&count).Error
-	return count, err
 }
